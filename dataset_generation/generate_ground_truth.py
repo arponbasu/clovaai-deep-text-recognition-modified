858f56a204b65d3f539e561fe23fd71831320073
arponbasu <200050013@iitb.ac.in>
2021-12-20 10:21:33 +0530

    Fixed bug

--- dataset_generation/generate_ground_truth.py
+++ dataset_generation/generate_ground_truth.py
@@ -1,4 +1,4 @@
-import random, os 
+import random, os, sys 
 random.seed(10)
 cwd = os.getcwd()
 if __name__ == "__main__":
@@ -6,10 +6,12 @@ if __name__ == "__main__":
     numString = str(sys.argv[1])
     path = cwd + '/data/PIL-' + numString + '/'
     ITER_LIM = int(sys.argv[1])
+    f = open(cwd + "/data/ground_truth_" + numString + ".txt", "w")
     for e in open(cwd + '/dataset_generation/ILP/ILP-' + numString).read().splitlines() :
         if iter >= ITER_LIM:
             break
         iter += 1
+        label = e.replace(' ', '')
         f.write('image_' + str(iter) + '.png' + '\t' + label + '\n')
     f.close()
 
