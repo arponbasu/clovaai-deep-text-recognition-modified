b902d025dc047def9b28de25732ad99270d05d03
arponbasu <200050013@iitb.ac.in>
2021-12-20 09:01:12 +0530

    Some more modifications

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing packages ..."
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 echo "Package installment complete"
 echo "Installing fonts; Requires sudo privileges ..."
 sudo cp -u dataset_generation/fonts/ /usr/local/share/fonts/
@@ -29,22 +29,28 @@ then
 else
 	echo "Your image directory already exists"
 fi
-echo "creating lmdb dataset, ensure that the data directory contains exactly one text file"
-INPUTDIR="data/PIL-${numImages}"
-python3 create_lmdb_dataset.py --inputPath "$INPUTDIR" --gtFile data/*.txt --outputPath result/
+echo "creating lmdb dataset ..."
+python3 create_lmdb_dataset.py --inputPath "data/PIL-${numImages}" --gtFile "$GTFILE" --outputPath result/
 echo "lmdb dataset created"
-read -p "Enter the number of iterations for training (default is 4000): " num_iter
+read -p "Number of CUDA visible devices: " CUDA_VISIBLE_DEVICES
 echo "Your GPU details ..."
-nvidia-smi
+nvidia-smi -L
+read -p "Enter the number of iterations for training (default is 4000): " num_iter
+read -p "Enter the path of your starting model (default is default_model/TPS-ResNet-BiLSTM-Attn.pth): " DEFAULT_MODEL
 echo "Beginning training ..."
-python3 train.py \
+python3 CUDA_VISIBLE_DEVICES = "$CUDA_VISIBLE_DEVICES" train.py \
 --train_data result/ --valid_data result/ \
 --select_data '/' --batch_ratio 1.0 --num_iter "$num_iter" \
 --Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction Attn \
 --character '0123456789ABCDEFGHIJKLMNOPQRSTVWXYZ'  \
---sensitive --saved_model 'default_model/TPS-ResNet-BiLSTM-Attn.pth' 
+--sensitive --saved_model "$DEFAULT_MODEL" 
 echo "Testing models against real life images"
-
+for filename in saved_models/TPS-ResNet-BiLSTM-Attn-Seed1111/*.pth; do
+    python3 CUDA_VISIBLE_DEVICES = "$CUDA_VISIBLE_DEVICES" demo.py \
+	--Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction Attn \
+	--image_folder test_images/ \
+	--saved_model "$file_name"
+done
 
 
 
